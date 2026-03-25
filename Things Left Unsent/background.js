725a25b14d0cdbd901ed9edd30c7ba7cdbe96a98
owibia <odien@usc.edu>
2026-03-24 17:58:12 -0700

    edit

---
+++ Things Left Unsent/background.js
@@ -0,0 +1,58 @@
+let video;
+let displaceColors;
+
+let displaceColorsSrc = `
+precision highp float;
+
+uniform sampler2D tex0;
+varying vec2 vTexCoord;
+
+vec2 zoom(vec2 coord, float amount) {
+  vec2 relativeToCenter = coord - 0.5;
+  relativeToCenter /= amount; // Zoom in
+  return relativeToCenter + 0.5; // Put back into absolute coordinates
+}
+
+void main() {
+  // Get each color channel using coordinates with different amounts
+  // of zooms to displace the colors slightly
+  gl_FragColor = vec4(
+    texture2D(tex0, vTexCoord).r,
+    texture2D(tex0, zoom(vTexCoord, 1.05)).g,
+    texture2D(tex0, zoom(vTexCoord, 1.1)).b,
+    texture2D(tex0, vTexCoord).a
+  );
+}
+`;
+
+function setup() {
+  createCanvas(700, 400, WEBGL);
+  video = createVideo(
+    ['assets/eating.mp4']
+  );
+  video.volume(0);
+  video.hide();
+  video.loop();
+  
+  displaceColors = createFilterShader(displaceColorsSrc);
+  
+  describe(
+    'A video of two different groups eating in Japan'
+  );
+
+  canvas.addClass('p5-background');
+}
+
+function draw() {
+  background(255);
+  push();
+  imageMode(CENTER);
+  image(
+    video,
+    0, 0, width, height,
+    0, 0, video.width, video.height,
+    COVER
+  );
+  pop();
+  filter(displaceColors);
+}
